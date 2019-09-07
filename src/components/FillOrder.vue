841954836d0ad3ece91c2254ae815d8ec6a1e613
kabl <blaser_k@gmx.ch>
2019-09-07 18:06:41 +0200

    fill order

--- src/components/FillOrder.vue
+++ src/components/FillOrder.vue
@@ -11,15 +11,36 @@
         <md-button class="md-raised md-primary" v-on:click="validateOrder">Validate Order</md-button>
       </md-field>
 
-      <OrderDetails v-if="order" :order="order" v-bind:key="order.hash"></OrderDetails>
+      <OrderDetails v-if="erc20Token" :order="order" :erc20Token="erc20Token" v-bind:key="order.hash"></OrderDetails>
 
       <md-field>
         <label>Taker Amount</label>
-        <md-input v-model="takerAmount" type="text"></md-input>
+        <md-input v-model="takerAmount" type="number" min="1"></md-input>
       </md-field>
 
+      <md-dialog :md-active.sync="showDialog">
+        <md-dialog-title>OTC Order Created</md-dialog-title>
+        <md-divider />
+        <div>
+          <md-content>You are about to {{orderType}} {{takerAmount}} XYZ Token for X WETH</md-content>
+        </div>
+        <md-divider />
+
+        <md-dialog-actions>
+          <md-button class="md-raised md-primary" @click="showDialog = false">Abort</md-button>
+          <md-button
+            class="md-raised md-accent"
+            v-on:click="fillOrder"
+          >Submit Fill Order</md-button>
+        </md-dialog-actions>
+      </md-dialog>
+
       <md-field>
-        <md-button class="md-raised md-accent" v-on:click="fillOrder">Fill Order</md-button>
+        <md-button
+          class="md-raised md-primary"
+          :disabled="order == null"
+          @click="showDialog = true"
+        >Prepare Fill Order</md-button>
       </md-field>
     </md-app-content>
   </md-app>
@@ -36,18 +57,31 @@ export default {
   data() {
     return {
       signedOrder: null,
-      takerAmount: null,
-      order: null
+      takerAmount: 1,
+      order: null,
+      erc20Token: null,
+      showDialog: false
     };
   },
+  computed: {
+    orderType: function() {
+      if (this.order === null) return "";
+      if (this.order.isSellOrder == "0") return "Buy";
+      if (this.order.isSellOrder == "1") return "Sell";
+      return "Error OrderType";
+    }
+  },
   methods: {
-    async fillOrder() {
-      console.log("Fill Order clicked");
-      //var order = JSON.parse(this.signedOrder);
-      await blockchain.fillOrder(this.order, this.takerAmount);
-    },
     async validateOrder() {
       this.order = JSON.parse(this.signedOrder);
+      this.erc20Token = await blockchain.getPersonalTokenInfo(this.order.token);
+      this.showDialog = false;
+    },
+    async fillOrder() {
+      console.log("Fill Order clicked");
+      await blockchain.fillOrder(this.order, this.takerAmount);
+      this.showDialog = false;
+      this.order = null;
     }
   }
 };
--- src/components/OrderDetails.vue
+++ src/components/OrderDetails.vue
@@ -7,7 +7,7 @@
       </md-table-row>
       <md-table-row>
         <md-table-cell>Order Type</md-table-cell>
-        <md-table-cell>{{ order.isSellOrder }}</md-table-cell>
+        <md-table-cell>{{ orderType }}</md-table-cell>
       </md-table-row>
       <md-table-row>
         <md-table-cell>Price</md-table-cell>
@@ -19,7 +19,7 @@
       </md-table-row>
       <md-table-row>
         <md-table-cell>Fill Option</md-table-cell>
-        <md-table-cell>{{ order.partialFillAllowed }}</md-table-cell>
+        <md-table-cell>{{ fillOption }}</md-table-cell>
       </md-table-row>
       <md-table-row>
         <md-table-cell>Lifetime</md-table-cell>
@@ -28,36 +28,38 @@
       <!-- <md-table-row>
         <md-table-cell>Hash</md-table-cell>
         <md-table-cell>{{ order.hash }}</md-table-cell>
-      </md-table-row> -->
+      </md-table-row>-->
       <!-- <md-table-row>
         <md-table-cell>Signature</md-table-cell>
         <md-table-cell>{{ order.sig }}</md-table-cell>
-      </md-table-row> -->
+      </md-table-row>-->
     </md-table>
   </div>
 </template>
 
 <script>
-import blockchain from "../js/blockchainInterface";
-
 export default {
   props: {
     order: {
       type: Object,
       required: true
+    },
+    erc20Token: {
+      type: Object,
+      required: true
     }
   },
-  data() {
-    return {
-      erc20Token: {}
-    };
-  },
-  created: async function() {
-    console.log("Created:", this.order.token);
-    this.erc20Token = await blockchain.getPersonalTokenInfo(this.order.token);
-  },
-  methods: {
-    async refresh() {}
+  computed: {
+    orderType: function() {
+      if (this.order.isSellOrder == 0) return "Buy Order";
+      if (this.order.isSellOrder == 1) return "Sell Order";
+      else return "Error OrderType";
+    },
+    fillOption: function() {
+      if (this.order.partialFillAllowed == 0) return "Require full order fill";
+      if (this.order.partialFillAllowed == 1) return "Allow partial order fill";
+      return "Error FillOption";
+    }
   }
 };
 </script>
