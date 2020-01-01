6e42563359da0971c051a835dd77e62c01b2cc17
kabl <kasimir@digitalizeit.io>
2020-01-01 15:43:37 +0100

    app structure

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,50 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Create Order</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Fill Order</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="indigo" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title>DEX3 - Simple, Fast, Reliable</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
-      <TokenContainer />
+      <v-container>
+        <TokenContainer />
 
-      <div>
-        <div>Create or Fill Order</div>
         <div>
-          <v-radio v-model="isMarketMaker" :value="true">Create a new Order</v-radio>
-          <v-radio v-model="isMarketMaker" :value="false">Fill an existing Order</v-radio>
+          <div>Create or Fill Order</div>
+          <div>
+            <v-radio v-model="isMarketMaker" :value="true">Create a new Order</v-radio>
+            <v-radio v-model="isMarketMaker" :value="false">Fill an existing Order</v-radio>
+          </div>
         </div>
-      </div>
 
-      <CreateOrder />
-      <FillOrder />
+        <CreateOrder />
+        <FillOrder />
+      </v-container>
     </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2020 DigitalizeIT GmbH</span>
+    </v-footer>
   </v-app>
 </template>
 
@@ -30,7 +57,8 @@ export default {
   name: "app",
   data() {
     return {
-      isMarketMaker: true
+      isMarketMaker: true,
+      drawer: null
     };
   },
   components: {
